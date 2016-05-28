3f9e195f9016d9983617e8556c0bc4c6fae3f9b0
Kai Wohlfahrt <kai.scorpio@gmail.com>
2016-05-28 20:36:32 +0100

    Add command buffer group
    Allows freeing of command buffer group as a unit. May be more efficient.

--- src/command_buffer.rs
+++ src/command_buffer.rs
@@ -44,10 +44,48 @@ impl<'a> PrimaryCommandBuffer<'a> {
 }
 
 impl<'a> Drop for PrimaryCommandBuffer<'a> {
-    // TODO: Add allocation group, so multiple can be deallocated together
     fn drop(&mut self) {
         unsafe {
             vkFreeCommandBuffers(*self.pool.device().handle(), *self.pool.handle(), 1, &self.handle)
+        }
+    }
+}
+
+pub struct PrimaryCommandBufferGroup<'a> {
+    handles: Vec<VkCommandBuffer>,
+    pool: &'a CommandPool<'a>,
+}
+
+
+impl <'a> PrimaryCommandBufferGroup<'a> {
+    pub fn allocate(pool: &'a CommandPool<'a>, n: u32) -> Result<Self, VkResult> {
+        let allocate_info = VkCommandBufferAllocateInfo{
+            s_type: VkStructureType::VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+            p_next: ptr::null(),
+            command_pool: *pool.handle(),
+            level: VkCommandBufferLevel::VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+            command_buffer_count: n,
+        };
+
+        let device = pool.device().handle();
+        let mut buffers = Vec::<VkCommandBuffer>::with_capacity(n as usize);
+        unsafe {
+            match vkAllocateCommandBuffers(*device, &allocate_info, buffers.as_mut_ptr()){
+                VkResult::VK_SUCCESS => {
+                    buffers.set_len(n as usize);
+                    Ok(PrimaryCommandBufferGroup{handles: buffers, pool: pool})
+                },
+                x => return Err(x)
+            }
+        }
+    }
+    pub fn len(&self) -> u32 {self.handles.len() as u32}
+}
+
+impl<'a> Drop for PrimaryCommandBufferGroup<'a> {
+    fn drop(&mut self) {
+        unsafe {
+            vkFreeCommandBuffers(*self.pool.device().handle(), *self.pool.handle(), self.handles.len() as u32, self.handles.as_ptr())
         }
     }
 }
@@ -81,5 +119,16 @@ mod test {
         let cmd_pool = CommandPool::new(&device, 0, CommandPoolCreateFlags::empty()).unwrap();
         let ref buf = PrimaryCommandBuffer::allocate(&cmd_pool, 1).unwrap()[0];
         buf.reset(CommandBufferResetFlags::empty());
+    }
+
+    #[test]
+    fn allocate_command_buffer_group() {
+        let instance = Instance::new(None, None).unwrap();
+        let device = {
+            let priorities = vec!((0, vec!(QueuePriority::from_float_clamped(1.0)))).into_iter().collect::<HashMap<_, _>>();
+            Device::new(&instance.devices().unwrap()[0], priorities).unwrap()
+        };
+        let cmd_pool = CommandPool::new(&device, 0, CommandPoolCreateFlags::empty()).unwrap();
+        assert!(PrimaryCommandBufferGroup::allocate(&cmd_pool, 1).unwrap().len() > 0);
     }
 }
