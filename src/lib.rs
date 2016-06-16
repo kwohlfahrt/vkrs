5b51fee51eac994156de520e7177f3e04470f30b
Kai Wohlfahrt <kjw53@cam.ac.uk>
2016-06-16 15:11:40 +0100

    Make clippy optional
    Should build on stable now, use "--features lint" to use clippy.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-#![feature(plugin)]
-#![plugin(clippy)]
+#![cfg_attr(feature="lint", feature(plugin))]
+#![cfg_attr(feature="lint", plugin(clippy))]
+#![cfg_attr(not(feature="lint"), allow(unknown_lints))]
 #[macro_use]
 // TODO: Track bitflags#20 for namespaced flags
 extern crate bitflags;
